3b6f954fd3c3aa1938b3e2b87b7432b6f462cb2b
Belsinghferrus <ferrusbelsingh@gmail.com>
2024-12-31 16:01:16 +0530

    socket

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,8 +13,11 @@ import {useThemeStore} from "./store/useThemeStore";
 
 const App = () => {
   const {theme} = useThemeStore()
-  const {authUser, checkAuth, isCheckingAuth} = useAuthStore()
+  const {authUser, checkAuth, isCheckingAuth, onlineUsers} = useAuthStore()
 
+
+  console.log({onlineUsers});
+  
   useEffect(()=>{
     checkAuth()
   },[checkAuth])
--- frontend/src/store/userAuthStore.js
+++ frontend/src/store/userAuthStore.js
@@ -1,24 +1,25 @@
 import {create} from 'zustand'
 import axiosInstance from '../lib/axios.js'
 import toast from 'react-hot-toast';
-import Message from './../../../backend/src/models/message.model';
+import {io } from 'socket.io-client'
 
+const BASE_URL = "http://localhost:5001/"
 
- const useAuthStore = create((set)=>({
+ const useAuthStore = create((set, get)=>({
     authUser: null,
     isSigningUp: false,
     isLoggingIn: false,
     isUpdatingProfile:false,
     onlineUsers:[],
-   
-
-
+    socket: null,
     isCheckingAuth:true,
 
     checkAuth: async()=>{
         try {
             const res = await axiosInstance.get('/auth/check');
             set({authUser: res.data})
+            get().connectSocket();
+
         } catch (error) {
             console.log("Error in checkAuth:", error);
             set({authUser:null})
@@ -36,7 +37,8 @@ import Message from './../../../backend/src/models/message.model';
             
             set({authUser: res.data})
             toast.success("Account created Successful")
-            
+            get().connectSocket();
+
         } catch (error) {
             console.log("Error in signUp", error);
             toast.error("Signup unsuccessful")   
@@ -51,6 +53,7 @@ import Message from './../../../backend/src/models/message.model';
             const res = await axiosInstance.post("/auth/login",data);
             set({authUser: res.data});
             toast.success("Login Successful");
+            get().connectSocket();
         } catch (error) {
             toast.error("invalid credential");  
         } finally{
@@ -64,6 +67,7 @@ import Message from './../../../backend/src/models/message.model';
             await axiosInstance.post("/auth/logout")
             set({authUser: null})
             toast.success("Logout Successful")
+            get().disconnectSocket();
         } catch (error) {
             console.log("error in logout", error);
             toast.error("Logout unsuccessful")
@@ -83,7 +87,27 @@ import Message from './../../../backend/src/models/message.model';
         }finally{
             set({isUpdatingProfile:false})
         }
-    }
+    },
+
+    connectSocket: () => {
+        const {authUser} = get()
+        if(!authUser || get().socket?.connected) return
+        const socket = io(BASE_URL, {
+            query: {
+                userId: authUser._id,
+        }})
+        socket.connect()
+        set({socket : socket})
+        socket.on("getOnlineUsers",  (userIds) => {
+            set({onlineUsers : userIds})
+        })
+    },
+
+    disconnectSocket: () => {
+        if(get().socket?.connected) get().socket.disconnect();
+    },
+
+
     
  }))
 
